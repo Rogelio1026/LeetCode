c6346a0133c3e0039a4a2a9cd7d586a48c5a26c5
Yuhao <rogelio1026zyh@gmail.com>
2016-07-16 20:27:36 +0800

    v2 runtime error

--- Sum_of_Two_Integer.py
+++ Sum_of_Two_Integer.py
@@ -5,9 +5,33 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        list = []
-        for i in range(a):
-            list.append(i)
-        for j in range(b):
-            list.append(j)
-        return len(list)
+        list_1 = []
+        list_a = []
+        list_b = []
+        list_c = []
+        c = min(a,b)
+        c = str(c)
+        list_c.append(c[0])
+        for i in range(abs(a)):
+            list_a.append(i)
+        for j in range(abs(b)):
+            list_b.append(j)
+        if a*b >= 0:
+            list_1.extend(list_a)
+            list_1.extend(list_b)
+            if a>0 or b>0:
+                return len(list_1)
+            elif a==0 and b==0:
+                return 0
+            else:
+                list_c.append(str(len(list_1)))
+                return int("".join(list_c))
+        else:
+            for x in max(list_a,list_b):
+                if x not in min(list_a,list_b):
+                    list_1.append(x)
+            if (a > b and abs(a) > abs(b)) or (b > a and abs(b) > abs(a)):
+                return len(list_1)
+            else:
+                list_c.append(str(len(list_1)))
+                return int("".join(list_c))
