854a501b57f07ec2e91e28af0f8230cf88557cd8
Yuhao <rogelio1026zyh@gmail.com>
2016-07-16 20:36:12 +0800

    v2 accepted

--- add_digits.py
+++ add_digits.py
@@ -6,14 +6,13 @@ class Solution(object):
         """
         a = num
         list_1 = []
-        list_2 = []
-        if num > 9:
+        while num > 9:
             num_str = str(num)
             list_1 = list(num_str)
+            list_2 = []
             for i in list_1:
                 i = int(i)
                 list_2.append(i)
-            print(list_2)
             a = sum(list_2)
             num = a
         return a
