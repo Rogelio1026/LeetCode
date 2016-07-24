a0f605cd8c7428c32e9bb01677744f65b2cdfe54
Yuhao <rogelio1026zyh@gmail.com>
2016-07-24 22:29:54 +0800

    ~x=-x-1,-1 is extra and have to be solved

--- Sum_of_Two_Integer/Sum_of_Two_Integer.py
+++ Sum_of_Two_Integer/Sum_of_Two_Integer.py
@@ -5,7 +5,15 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        if abs(a) == abs(b) and a * b <= 0:
+        if a*b<0 and abs(min(a,b))<abs(max(a,b)):
+            a=~a
+            b=~b
+            while (a & b) != 0:
+                c = (a & b) << 1
+                b = a ^ b
+                a = c
+            return ~(a | b)-1
+        elif a*b<=0 and abs(a)==abs(b):
             return 0
         else:
             while (a & b) != 0:
