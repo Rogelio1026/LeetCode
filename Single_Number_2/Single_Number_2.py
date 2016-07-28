7b1bfdfd2f0a1bf4c31e1b889e9654816179205c
Yuhao <rogelio1026zyh@gmail.com>
2016-07-28 20:40:55 +0800

    58ms 62.44%

--- Single_Number_2/Single_Number_2.py
+++ Single_Number_2/Single_Number_2.py
@@ -2,16 +2,18 @@ class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
-        :rtype: int
+        :rtype: List[int]
         """
         dict = {}
+        list = []
         for n in nums:
             if n not in dict:
                 dict[n]=1
             else:
-                dict[n]=3
+                dict[n]=2
         for n in dict:
             if dict[n]==1:
-                return n
+                list.append(n)
+        return list
         # time - O(n)
         # space - O(n)
