85978038073d5916676fff474d7d25079b478c1c
Yuhao <rogelio1026zyh@gmail.com>
2016-08-11 23:17:44 +0800

    binary_search finished

--- Interview/Binary_Search.py
+++ Interview/Binary_Search.py
@@ -2,18 +2,22 @@ def Binary_search(number,list_1):
     """
 
     :param number: int
-    :return: tuple
+    :return: int
     """
     i = 0
     mid = len(list_1)/2
     if number > list_1[-1]:
         return len(list_1)-1
-    if number < list_1[0]:
+    elif number < list_1[0]:
         return -1
-    if number == list_1[mid]:
+    elif number == list_1[mid]:
         return mid
     elif number > list_1[mid]:
         i += mid
-        Binary_search(number,list_1[mid:])
+        return i + Binary_search(number,list_1[mid:])
     else:
-        Binary_search(number,list_1[:mid])
+        return i + Binary_search(number,list_1[:mid])
+
+if __name__ == '__main__':
+    print(Binary_search(9,(1,3,5,7,9)))
+    assert(Binary_search(5,(1,3,7))==1)
