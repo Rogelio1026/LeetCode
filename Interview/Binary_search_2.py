b8600bbb71b8db9c003d1d8cd5c69445544aaf4e
Yuhao <rogelio1026zyh@gmail.com>
2016-08-16 00:58:19 +0800

    binary search 2

--- Interview/Binary_search_2.py
+++ Interview/Binary_search_2.py
@@ -1,17 +1,18 @@
 def Binary_search(number,list_1):
-    mid = len(list_1)/2
-    i = 0
-    while number > list_1[i]:
-        if number > list_1[-1] or number < list_1[i+1]:
-            return i
-        elif number == list_1[mid]:
-            i = mid
-            return i
+    first = 0
+    last = len(list_1)-1
+    while first <= last:
+        mid = (first+last)/2
+        if number == list_1[mid]:
+            return mid
         elif number > list_1[mid]:
-            list_1 = list_1[mid:]
-            i += mid
+            first = mid + 1
         elif number < list_1[mid]:
-            list_1 = list_1[:mid]
+            last = mid -1
+    if number < list_1[0]:
+        return -1
+    else:
+        return first-1
 
 if __name__ == '__main__':
-    print(Binary_search(5,[1,3,5]))
+    print(Binary_search(5,[1,3,5,7,9]))
